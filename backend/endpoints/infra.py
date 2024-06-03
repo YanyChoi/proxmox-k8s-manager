15fc590a59fd9303770b2d9a567547a8533a338a
Yany Choi <dydghks0818@naver.com>
2024-06-03 16:03:43 +0900

    add apply endpoints

--- backend/endpoints/infra.py
+++ backend/endpoints/infra.py
@@ -5,14 +5,24 @@ from ansible.generator import PlaybookGenerator
 from ansible.play import play
 router = APIRouter(tags=["infra"])
 
-@router.post("/vm_template")
-async def create_vm_template(config: Config = Depends(get_config)) -> Response:
+@router.post("/vm_template/plan")
+async def plan_vm_template(config: Config = Depends(get_config)) -> Response:
     playbook_path = PlaybookGenerator().create_vm_template(config.template)
-    # play(playbook_path)
     return playbook_path
 
-@router.post("/cluster")
-async def create_vm(config: Config = Depends(get_config)) -> Response:
+@router.post("/cluster/plan")
+async def plan_vm(config: Config = Depends(get_config)) -> Response:
     playbook_path = PlaybookGenerator().create_vm(config)
-    # play(playbook_path)
     return playbook_path
+
+@router.post("/vm_template/apply")
+async def apply_vm_template(config: Config = Depends(get_config)) -> Response:
+    playbook_path = PlaybookGenerator().create_vm_template(config.template)
+    play(playbook_path)
+    return Response(status_code=200)
+
+@router.post("/cluster/apply")
+async def apply_vm(config: Config = Depends(get_config)) -> Response:
+    playbook_path = PlaybookGenerator().create_vm(config)
+    play(playbook_path)
+    return Response(status_code=200)
