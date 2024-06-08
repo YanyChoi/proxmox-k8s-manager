038e14b10d15c50f76962718d645ef7eaeb629aa
Yany Choi <dydghks0818@naver.com>
2024-06-08 22:57:27 +0900

    fix script variables

--- backend/ansible/data/scripts/router.sh
+++ backend/ansible/data/scripts/router.sh
@@ -7,8 +7,8 @@ sudo apt-get update -y
 echo "[TASK 2] Set forwarding & NAT"
 sudo sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|g' /etc/sysctl.conf
 sudo sysctl -p
-sudo iptables -t nat -A POSTROUTING -o ens19 -j SNAT --to-source $network_router_ip
-sudo iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source $public_ip
+sudo iptables -t nat -A POSTROUTING -o ens19 -j SNAT --to-source $NETWORK_ROUTER_IP
+sudo iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to-source $PUBLIC_IP
 
 echo "[TASK 3] Set Port Forwarding"
 
@@ -17,23 +17,15 @@ sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-desti
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 6443 -j DNAT --to-destination $LB_IP:6443
 sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 1194 -j DNAT --to-destination $VPN_IP:1194
 
-for port in $tcp_ports; do
-  sudo iptables -t nat -A PREROUTING -i eth0 -p tcp --dport $port -j DNAT --to-destination $network_router_ip:$port
-done
-
-for port in $udp_ports; do
-  sudo iptables -t nat -A PREROUTING -i eth0 -p udp --dport $port -j DNAT --to-destination $network_router_ip:$port
-done
-
 echo "[TASK 2] Install FRRouting & dhcpd"
 sudo apt-get install -y frr isc-dhcp-server
 
 echo "[TASK 3] Configure DHCP Server"
 sudo tee /etc/dhcp/dhcpd.conf <<EOF
-subnet $network_cidr netmask $network_mask {
-  range $network_dhcp_start $network_dhcp_end;
-  option routers $network_router_ip;
-  option domain-name-servers $network_dns_primary, $network_dns_secondary;
+subnet $NETWORK_CIDR netmask $NETWORK_MASK {
+  range $NETWORK_DHCP_START $NETWORK_DHCP_END;
+  option routers $NETWORK_ROUTER_IP;
+  option domain-name-servers $NETWORK_DNS_PRIMARY, $NETWORK_DNS_SECONDARY;
   default-lease-time 600;
   max-lease-time 7200;
 }
@@ -43,23 +35,23 @@ sudo sed -i 's|zebra=no|zebra=yes|g; s|ospfd=no|ospfd=yes|g; s|bgpd=no|bgpd=yes|
 sudo tee /etc/frr/frr.conf <<EOF
 frr version 8.1
 frr defaults traditional
-hostname $hostname
+hostname router
 log syslog informational
 service integrated-vtysh-config
 !
 interface ens19
-  ip address $network_router_ip/$network_cidr
+  ip address $NETWORK_ROUTER_IP/$NETWORK_CIDR
   no shutdown
   exit
 !
 router bgp $bgp_as
-  bgp router-id $network_router_ip
-  neighbor $network_router_ip remote-as $bgp_as
-  neighbor $network_router_ip update-source ens19
-  neighbor $network_router_ip ebgp-multihop
-  neighbor $network_router_ip next-hop-self
-  neighbor $network_router_ip activate
-  network $network_cidr
+  bgp router-id $NETWORK_ROUTER_IP
+  neighbor $NETWORK_ROUTER_IP remote-as $bgp_as
+  neighbor $NETWORK_ROUTER_IP update-source ens19
+  neighbor $NETWORK_ROUTER_IP ebgp-multihop
+  neighbor $NETWORK_ROUTER_IP next-hop-self
+  neighbor $NETWORK_ROUTER_IP activate
+  network $NETWORK_CIDR
 exit
 !
 line vty
